18d2a2a54a71742061642d69c7b4a33a9f2ea4db
VK132211 <m.vk.80899@gmail.com>
2024-12-23 13:58:27 +0530

    add axios configuration and authentication API requests

---
+++ src/hooks/apis/auth/useSignup.js
@@ -0,0 +1,26 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { signUpRequest } from "@/apis/auth";
+
+export const useSignup = () => {
+  const {
+    isPending,
+    isSuccess,
+    error,
+    mutate: signupMutation,
+  } = useMutation({
+    mutationFn: signUpRequest,
+    onSuccess: (data) => {
+      console.log("Successfull Signup", data);
+    },
+    onError: (error) => {
+      console.log("Failed to Signup", error);
+    },
+  });
+  return {
+    isPending,
+    isSuccess,
+    error,
+    signupMutation,
+  };
+};
