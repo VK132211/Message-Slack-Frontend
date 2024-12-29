7c3375ba2036ccfd40be302265d4c3837489ef74
VK132211 <m.vk.80899@gmail.com>
2024-12-29 15:46:49 +0530

    add channel management: implement CreateChannelModal and SideBarItem components for channel creation and navigation

--- src/components/organisms/Workspace/WorkspacePanel.jsx
+++ src/components/organisms/Workspace/WorkspacePanel.jsx
@@ -1,11 +1,14 @@
 import { WorkspacePanelHeader } from "@/components/molecules/Workspace/WorkspacePanelHeader";
 import { useGetWorkspaceById } from "@/hooks/apis/workspaces/useGetWorkspaceById";
-import { AlertTriangleIcon, Loader } from "lucide-react";
+import { AlertTriangleIcon, HashIcon, Loader, MessageSquareTextIcon, SendHorizonalIcon } from 'lucide-react';
 import { useParams } from "react-router-dom";
-
+import { SideBarItem } from "@/components/atoms/SideBarItem/SideBarItem";
+import { WorkspacePanelSection } from '@/components/molecules/Workspace/WorkspacePanelSection';
+import { useCreateChannelModal } from '@/hooks/context/useCreateChannelModal';
 export const WorkspacePanel = () => {
   const { workspaceId } = useParams();
   const { workspace, isFetching, isSuccess } = useGetWorkspaceById(workspaceId);
+  const { setOpenCreateChannelModal } = useCreateChannelModal();
   if (isFetching) {
     return (
       <div className="flex flex-col gap-y-2 items-center justify-center h-full text-white">
@@ -25,6 +28,20 @@ export const WorkspacePanel = () => {
   return (
     <div className="flex flex-col h-full bg-slack-medium">
       <WorkspacePanelHeader workspace={workspace} />
+      <div className="flex flex-col px-2 mt-3">
+        <SideBarItem label="Threads" icon={MessageSquareTextIcon} id="threads" variant="active" />
+        <SideBarItem label="Drafts & Sent" icon={SendHorizonalIcon} id="drafts" variant="default" />
+      </div>
+      <WorkspacePanelSection
+        label={"Channels"}
+        onIconClick={() => {
+          setOpenCreateChannelModal(true);
+        }}
+      >
+        {workspace?.channels?.map((channel) => {
+          return <SideBarItem key={channel._id} icon={HashIcon} label={channel.name} id={channel._id} />;
+        })}
+      </WorkspacePanelSection>
     </div>
   );
 };
--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -2,5 +2,6 @@ import {AuthContextProvider} from "./AuthContext";
 import combineContext from "@/utils/combineContext";
 import { CreateWorkspaceContextProvider } from "./CreateWorkspaceContext";
 import { WorkspacePreferencesModalContextProvider } from "./WorkspacePreferencesModalContext";
+import { CreateChannelContextProvider } from "./CreateChannelContext";
 
-export const AppContextProvider = combineContext(AuthContextProvider, CreateWorkspaceContextProvider, WorkspacePreferencesModalContextProvider);
+export const AppContextProvider = combineContext(AuthContextProvider, CreateWorkspaceContextProvider, WorkspacePreferencesModalContextProvider, CreateChannelContextProvider);
